460d477f7194e64e51714af0fdb83c09ef5ff55d
Ankush Kumar <ankush07614902024@msijanakpuri.com>
2026-07-19 01:29:33 +0530

    port fix

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
+const PORT = process.env.PORT || 3000;
 
 const serverStart = async () => {
     await connectDB();
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
         console.log(`Server started on port 3000`);
     });
 };
